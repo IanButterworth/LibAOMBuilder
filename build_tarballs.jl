66fe12db6486d1c6414ac07054211b162f15a925
Ian <i.r.butterworth@gmail.com>
2019-06-16 00:40:12 -0400

    Initial commit

---
+++ build_tarballs.jl
@@ -0,0 +1,37 @@
+# Note that this script can accept some limited command-line arguments, run
+# `julia build_tarballs.jl --help` to see a usage message.
+using BinaryBuilder
+name = "LibAOM"
+version = v"1.0"
+# Collection of sources required to build imagemagick
+
+sources = [
+    "https://aomedia.googlesource.com/aom",
+]
+
+# Bash recipe for building across all platforms
+script = raw"""
+cd $WORKSPACE/srcdir
+cd aom/
+cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_SHARED_LIBS=1
+make -j${ncore}
+make install
+exit
+"""
+
+# These are the platforms we will build for by default, unless further
+# platforms are passed in on the command line
+platforms = supported_platforms()
+
+# The products that we will ensure are always built
+products(prefix) = [
+]
+
+# Dependencies that must be installed before this package can be built
+dependencies = [
+
+]
+
+
+# Build the tarballs, and possibly a `build.jl` as well.
+build_tarballs(ARGS, name, version, sources, script, platforms, products, dependencies)
