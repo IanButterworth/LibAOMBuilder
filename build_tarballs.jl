fd1b7f498692158b7b061ff20d33b757cbed330e
Ian <i.r.butterworth@gmail.com>
2019-06-16 01:35:54 -0400

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -7,15 +7,16 @@ version = v"1.0.0"
 
 sources = [
     "http://www.andrews-corner.org/downloads/SBo/libaom-1.0.0.r1804.gcb43f766c.tar.gz" =>
-  "365ebf37fdbe6265c4e2a6815c9d19b0caa0a1279a04e5b0eea8fc77e3847420",
+    "365ebf37fdbe6265c4e2a6815c9d19b0caa0a1279a04e5b0eea8fc77e3847420",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd aom
+mkdir aom_build
+cd aom_build
 apk install yasm
-cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_SHARED_LIBS=1
+cmake $WORKSPACE/srcdir/libaom-1.0.0.r1804.gcb43f766c -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_SHARED_LIBS=1
 make -j${ncore}
 make install
 exit
@@ -27,6 +28,7 @@ platforms = supported_platforms()
 
 # The products that we will ensure are always built
 products(prefix) = [
+      LibraryProduct(prefix, "addition", :addition),
 ]
 
 # Dependencies that must be installed before this package can be built
