23f794bfdf0036b39bcf8a14b5bffd8c28743e6d
Ian <i.r.butterworth@gmail.com>
2019-06-16 00:47:45 -0400

    Update build_tarballs.jl

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -6,13 +6,15 @@ version = v"1.0"
 # Collection of sources required to build imagemagick
 
 sources = [
-    "https://aomedia.googlesource.com/aom",
+    "$WORKSPACE/srcdir/aom",
 ]
 
 # Bash recipe for building across all platforms
 script = raw"""
 cd $WORKSPACE/srcdir
-cd aom/
+git clone https://aomedia.googlesource.com/aom
+cd aom
+apk install yasm
 cmake -DCMAKE_INSTALL_PREFIX=$prefix -DCMAKE_TOOLCHAIN_FILE=/opt/$target/$target.toolchain -DBUILD_SHARED_LIBS=1
 make -j${ncore}
 make install
